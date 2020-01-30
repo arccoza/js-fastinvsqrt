5a7451c8945b5b7524cc05ef473627c54e5ee7f9
Adrien <adrien@arccoza.com>
2020-01-30 07:21:53 +0000

    Update fn with global typed array, still slow, not thread safe

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const m = 0x5F375A86
+const m = 0x5F375A86,
+  buffer = new ArrayBuffer(4),
+	view = new DataView(buffer)
+
 function fastInvSqrt (n) {
-	var f, n2 = n * 0.5, th = 1.5,
-		buffer = new ArrayBuffer(4),
-		view = new DataView(buffer)
+	var f, n2 = n * 0.5, th = 1.5
+		// buffer = new ArrayBuffer(4),
+		// view = new DataView(buffer)
 	view.setFloat32(0, n)
 	view.setUint32(0, m - (view.getUint32(0) >> 1))
 	f = view.getFloat32(0)
