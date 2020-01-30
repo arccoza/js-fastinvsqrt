7a9f3c1f75eeb579eb8913f85b9c7b36d7bedc84
Adrien <adrien@arccoza.com>
2020-01-30 07:17:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const m = 0x5F375A86
+function fastInvSqrt (n) {
+	var f, n2 = n * 0.5, th = 1.5,
+		buffer = new ArrayBuffer(4),
+		view = new DataView(buffer)
+	view.setFloat32(0, n)
+	view.setUint32(0, m - (view.getUint32(0) >> 1))
+	f = view.getFloat32(0)
+	f *= (th - (n2 * f * f))
+	f *= (th - (n2 * f * f))
+  return f
+}
+
+console.log(fastInvSqrt(1.6))
+console.log(1/Math.sqrt(1.6))
