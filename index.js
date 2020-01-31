7b19ec3e3c98b0b4b09e46aadcb5e8843ae733d1
Adrien <adrien@arccoza.com>
2020-01-31 17:03:01 +0900

    Fix indents

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const m = 0x5F375A86,
   buffer = new ArrayBuffer(4),
-	view = new DataView(buffer)
+  view = new DataView(buffer)
 
 function fastInvSqrt (n) {
-	var f, n2 = n * 0.5, th = 1.5
-		// buffer = new ArrayBuffer(4),
-		// view = new DataView(buffer)
-	view.setFloat32(0, n)
-	view.setUint32(0, m - (view.getUint32(0) >> 1))
-	f = view.getFloat32(0)
-	f *= (th - (n2 * f * f))
-	f *= (th - (n2 * f * f))
+  var f, n2 = n * 0.5, th = 1.5
+    // buffer = new ArrayBuffer(4),
+    // view = new DataView(buffer)
+  view.setFloat32(0, n)
+  view.setUint32(0, m - (view.getUint32(0) >> 1))
+  f = view.getFloat32(0)
+  f *= (th - (n2 * f * f))
+  f *= (th - (n2 * f * f))
   return f
 }
 
